11b407e032c140136d48b5ea047eab43a926cf6c
Daniil Dyakov <danya-dyakov@yandex.ru>
2022-11-18 23:07:34 +0300

    закончил дз

--- s2/z15/z15.cs
+++ s2/z15/z15.cs
@@ -3,3 +3,15 @@
 //7 -> да
 //1 -> нет
 
+System.Console.Write("Введите цифру, обозначающую день недели:  ");
+
+int numb=Convert.ToInt32(Console.ReadLine());
+
+if (numb==6|numb==7)
+{
+    Console.Write($"День недели    В Ы Х О Д Н О Й");
+}
+else
+{
+    Console.Write($"День недели   Р А Б О Ч И Й");
+}
